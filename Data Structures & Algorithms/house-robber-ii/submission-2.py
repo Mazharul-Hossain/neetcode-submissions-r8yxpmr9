88c5658e4e96f3ced2fb06467799cd5fb015b7a9
Mazharul Hossain <2561458+Mazharul-Hossain@users.noreply.github.com>
2026-04-12 16:12:49 -0500

    Add: house-robber-ii - submission-2

---
+++ Data Structures & Algorithms/house-robber-ii/submission-2.py
@@ -0,0 +1,48 @@
+class Solution:
+    def rob_I(self, nums: List[int]) -> int:        
+        N = len(nums)
+        
+        if N == 0:
+            return 0
+            
+        elif N == 1:
+            return nums[0]
+            
+        # Step 2: Define state (MOST IMPORTANT)
+        # Let dp[i] represent how much we can steal at house i - 1?
+        dp = [0] * (N + 1)
+
+        # Step 4: Base cases
+        # Steal the first house
+        dp[1] = nums[0]
+
+        # Step 5: Order of computation
+        # We compute from left --> right because we can take n only once
+        for i in range(2, N + 1):
+
+            # Step 3: Recurrence (transition)
+            # At each step, I have two choices:
+            #           leave it  or take it
+            dp[i] = max(dp[i - 1], nums[i - 1] + dp[i - 2])
+            
+            # print(dp)            
+        return dp[N]
+
+    def rob(self, nums: List[int]) -> int:        
+        # Step 1: Identify pattern: 
+        # This looks like a DP problem because 
+        # decisions at index i depend on previous subproblems.
+        N = len(nums)
+        if N == 0:
+            return 0
+
+        elif N == 1:
+            return nums[0]
+
+        # Step 6: Optimization
+        # Using 1D DP
+        # Step 7: Complexity 
+        # - Time: O(N * 2)
+        # - Space O(N * 2)
+
+        return max(self.rob_I(nums[:-1]), self.rob_I(nums[1:]) )
