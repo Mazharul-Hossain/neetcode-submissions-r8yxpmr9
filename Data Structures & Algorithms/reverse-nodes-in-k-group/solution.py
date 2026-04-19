7b7dc71030a8a73f23eaa3b933b8c0d751f5abfc
My Name <my-email@example.com>
2026-04-19 18:31:31 -0500

    Added reverse-nodes-in-k-group on 2026-04-19

--- Data Structures & Algorithms/reverse-nodes-in-k-group/solution.py
+++ Data Structures & Algorithms/reverse-nodes-in-k-group/solution.py
@@ -1,3 +1,17 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+
+class Solution:
+    def getKCandidate(self, head: Optional[ListNode], k: int) -> Optional[ListNode]:
+        while k > 0 and head:
+            k -= 1
+            head = head.next
+        
+        return head
+
     def reverseKGroup(self, head: Optional[ListNode], k: int) -> Optional[ListNode]:
         prevKGroup = dummy = ListNode(0, next=head)
         # print(prevKGroup.val, prevKGroup.next.val)
@@ -7,28 +21,15 @@
             if KCandidate is None:
                 break
             groupNext = KCandidate.next
-
-        return head
-            k -= 1
-            head = head.next
-        
             # print("KCandidate", KCandidate.val)
             
-            prev, curr = KCandidate.next, prevKGroup.next
-            while curr != KCandidate:
+            # reverse group
+            prev, curr = groupNext, prevKGroup.next
+            while curr != groupNext:
                 temp = curr.next
                 curr.next = prev
 
                 prev = curr
                 curr = temp
                 # print(prev.val, curr.val)
-            
-            # print(prevKGroup.val, prevKGroup.next.val)
-            temp_head = prevKGroup.next
-
-            curr.next = prev
-            prevKGroup.next = curr
-            prevKGroup = temp_head
-        
-        return dummy.next
-        
+                
