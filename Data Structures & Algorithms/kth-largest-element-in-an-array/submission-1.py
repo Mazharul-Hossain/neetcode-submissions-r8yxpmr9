709a74954b50b248c4a1b395cb4574d3ed184fcc
Mazharul Hossain <2561458+Mazharul-Hossain@users.noreply.github.com>
2026-04-20 09:24:03 -0500

    Add: kth-largest-element-in-an-array - submission-1

---
+++ Data Structures & Algorithms/kth-largest-element-in-an-array/submission-1.py
@@ -0,0 +1,38 @@
+import random
+
+
+class Solution:
+    def findKthLargest(self, nums: List[int], k: int) -> int:
+        k = len(nums) - k  # convert to kth smallest
+
+        def partition(left: int, right: int, pivot_index: int) -> int:
+            pivot = nums[pivot_index]
+
+            # move pivot to end
+            nums[right], nums[pivot_index] = nums[pivot_index], nums[right]
+
+            store_index = left
+            for i in range(left, right):
+                if nums[i] < pivot:
+                    nums[i], nums[store_index] = nums[store_index], nums[i]
+                    store_index += 1
+                
+            # move pivot to correct place
+            nums[right], nums[store_index] = nums[store_index], nums[right]
+            return store_index
+
+        def quickSelect(left: int, right: int):
+            while left <= right:
+                pivot_index = random.randint(left, right)
+                c_pivot_index = partition(left, right, pivot_index)
+
+                if c_pivot_index == k:
+                    return nums[c_pivot_index]
+                
+                elif c_pivot_index < k:
+                    left = c_pivot_index + 1
+                
+                else:
+                    right = c_pivot_index - 1
+
+        return quickSelect(0, len(nums) - 1)
