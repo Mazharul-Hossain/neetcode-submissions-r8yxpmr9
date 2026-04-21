5f1276e5a19350d15e02d9dbc281884726e8224b
My Name <my-email@example.com>
2026-04-21 17:18:39 -0500

    Added palindromic-substrings on 2026-04-21

---
+++ Data Structures & Algorithms/palindromic-substrings/solution.py
@@ -0,0 +1,34 @@
+
+        answer = 0
+        # Step 5: Order of computation
+        # We compute from left --> right and top <-- bottom because we can take compute substrings only once
+        for i in range(N - 1, -1, -1):
+            # Step 4: Base cases
+            # each letter is 1 len palindrom
+
+            for j in range(i, N):
+                # Step 3: Recurrence (transition)
+                # At each step, I have one choices:
+                # palindrom and previous also palindrom
+                # print(i + 1, j - 1)
+                if s[i] == s[j] and (j - i <= 2 or dp[i + 1][j - 1]):
+                    dp[i][j] = True
+                    answer += 1
+                    # temp_len = j - i + 1
+                    # if ans_len < temp_len:
+                    #     ans_indx = i
+                    #     ans_len = temp_len
+            
+            # print(dp)
+            # print()
+
+        # Step 6: Optimization
+        # Using 1D DP
+        # Step 7: Complexity 
+        # - Time: O(N * N)
+        # - Space O(N * N)
+
+        # return s[ans_indx: ans_indx + ans_len]
+        return answer
+
+        
