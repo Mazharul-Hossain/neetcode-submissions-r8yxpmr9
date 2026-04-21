a4bc8b5666b8b5202f71f82aca82614fc0535e97
My Name <my-email@example.com>
2026-04-21 16:32:51 -0500

    Added climbing-stairs on 2026-04-21

---
+++ Data Structures & Algorithms/climbing-stairs/solution.py
@@ -0,0 +1,8 @@
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        dp = [1, 1, 2]
+        for _ in range(3, n + 1):
+            dp.append(dp[-1] + dp[-2])
+        
+        return dp[n]
+        
