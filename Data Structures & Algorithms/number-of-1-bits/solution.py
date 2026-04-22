c53ba4c5e6df6fcea9f67a5da0e2a9effc0a2853
My Name <my-email@example.com>
2026-04-22 12:34:59 -0500

    Added number-of-1-bits on 2026-04-22

--- Data Structures & Algorithms/number-of-1-bits/solution.py
+++ Data Structures & Algorithms/number-of-1-bits/solution.py
@@ -1,12 +1,14 @@
-class Solution:
-    def hammingWeight(self, n: int) -> int:
         answer = 0
         while n:
-            if n & 1:
-                answer += 1
+            # if n & 1:
+            #     answer += 1
             
-            n = n >> 1
+            # n = n >> 1
             # print(bin(n))
         
+            n = n & (n - 1)
+            answer += 1
         return answer
         
+class Solution:
+    def hammingWeight(self, n: int) -> int:
