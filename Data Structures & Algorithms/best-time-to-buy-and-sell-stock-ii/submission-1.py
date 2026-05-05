3d46227c1e3fa0d8c41eec9659bc9f420591f3fd
Mazharul Hossain <2561458+Mazharul-Hossain@users.noreply.github.com>
2026-05-05 11:15:44 -0500

    Add: best-time-to-buy-and-sell-stock-ii - submission-1

---
+++ Data Structures & Algorithms/best-time-to-buy-and-sell-stock-ii/submission-1.py
@@ -0,0 +1,16 @@
+from functools import lru_cache
+
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        @lru_cache(maxsize=2048)
+        def rec(i, bought):
+            if i == len(prices):
+                return 0
+            res = rec(i + 1, bought)
+            if bought:
+                res = max(res, prices[i] + rec(i + 1, False))
+            else:
+                res = max(res, -prices[i] + rec(i + 1, True))
+            return res
+        return rec(0, False)
+        
