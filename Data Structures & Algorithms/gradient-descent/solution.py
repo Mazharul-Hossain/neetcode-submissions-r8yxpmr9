bc23745aea352643b5a46e4059cf464e1dd80b4f
My Name <my-email@example.com>
2026-05-07 17:39:36 -0500

    Added gradient-descent on 2026-05-07

--- Data Structures & Algorithms/gradient-descent/solution.py
+++ Data Structures & Algorithms/gradient-descent/solution.py
@@ -4,7 +4,10 @@ class Solution:
         # Derivative:         f'(x) = 2x
         # Update rule:        x = x - learning_rate * f'(x)
         # Round final answer to 5 decimal places
+        minimizer = init
+
         for _ in range(iterations):
-            init -= learning_rate * 2 * init
+            derivative = 2 * minimizer
+            minimizer = minimizer - learning_rate * derivative
         
-        return round(init, 5)
+        return round(minimizer, 5)
